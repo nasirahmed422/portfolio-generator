37df101ccb7ac2377a908b2363e043926529c745
Nasir Ahmed <nahmed@utilant.com>
2021-11-09 12:17:11 -0500

    p2

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-//Takes user input puts them into an array and then returns from second one on because of first two being file paths.
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-//Same as above
-/* const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-}; */
+const profileDataArgs = process.argv.slice(2);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
